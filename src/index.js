ecb8f5a3e534ba72d4753c46dd3366775cf86906
Maurici <hello@mauriciabad.com>
2019-11-08 15:20:10 +0100

    Serve public folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const path    = require('path');
+const app     = express();
+const http    = require('http').createServer(app);
+const io      = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
