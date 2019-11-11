8bff72088656c7946a66d0e202d71205743756a0
Maurici <hello@mauriciabad.com>
2019-11-11 21:24:06 +0100

    Typo fix

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* - - - - Inicializalize variables - - - - */
+/* - - - - Initialize variables - - - - */
 const express = require('express');
 const app     = express();
 const http    = require('http').createServer(app);
